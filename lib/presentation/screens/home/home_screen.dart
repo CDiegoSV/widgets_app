9c718d8f1f3250d10eac597f8ccc7daf70400abd
CDiegoSV <cdsalgadov@gmail.com>
2025-10-10 11:05:53 -0600

    Named Routes Implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -2,8 +2,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu/menu_item.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -54,6 +58,7 @@ class _CustomListTile extends StatelessWidget {
       onTap: () {
         // Navigator.pushNamed(context, menuItem.link);
         context.push(menuItem.link);
+        // context.pushNamed(CardsScreen.name);
       },
     );
   }
